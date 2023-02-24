e9028a13bac6d7a2175b4973795d8b95301d93b1
Shubhangi Sharma <98348687+shubhangi-24@users.noreply.github.com>
2023-02-25 01:31:45 +0530

    Add files via upload

---
+++ Shubhangi Portfolio/script.js
@@ -0,0 +1,20 @@
+var typed=new Typed(".multiple-text",{
+    strings:["Engineering Student","Aspiring Data Scientist","FrontEnd Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+})
+
+
+
+// <script>
+//     var sidemenu = document.getElementById("sidemenu");
+
+//     function openmenu(){
+//         sidemenu.style.right = "0";
+//     }
+//     function closemenu() {
+//             sidemenu.style.right = "-200px";
+//         }
+//     </script>
